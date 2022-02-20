d32708270f4d214dc5d9751d46c965b4ebcada79
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2022-02-20 15:27:00 +0300

    sidebar

--- src/components/cards/Sidebar.js
+++ src/components/cards/Sidebar.js
@@ -1,32 +1,36 @@
 import React from 'react';
+import '../../styles/Sidebar.css'
+import dash from '../../images/dashboard.svg'
 
 const Sidebar = () => {
     return (
         <div className="w-25 sidebar-custom-wrapper">
+        
         <div className='sidebar-custom'>
-            {/* Taşı */}
-           
-                    <div className='sidebar-logo-wrapper d-flex flex-column align-items-center justify-content-center'>
+            <div className='sidebar-custom__side'>
+                <h2 className='sidebar-custom__text logo'>
+                    arotolia
+                </h2>
+            </div>
+                <div className='sidebar-custom__main d-flex flex-column align-items-center'>
+                    <div className='sidebar-custom__upper d-flex flex-column'>
+                        <div className='sidebar-custom__user-name-wrapper'>
+                            User N.
+                        </div>
+                   
                 
-                </div>
-               
-                <ul className='sidebar-inner'>
-               
-                <li onClick={()=>window.open("https://zapier.com/apps/brieferr/integrations", "_blank")}
-                        className='item mb-2 bold-small-paragraph'
-                        >
-                  
-                            <div id="title">Integrations</div>
-                          
-                        </li>
-                        <li onClick={()=>window.open("https://blog.brieferr.com", "_blank") }
-                        className='item mb-2 bold-small-paragraph'
-                        >
-                            <div id="title">Blog</div>
-                          
-                        </li>
-                </ul> 
+                        <ul className='sidebar-custom__middle text-left sidebar-cutom__items-wrapper'>
+                            <a className='big-p' href=""><li><img src={dash}></img>DASHBOARD</li></a>
+                            <a className='big-p' href=""><li>LIBRARY</li></a>
+                            <a className='big-p' href=""><li>PATIENTS</li></a>
+                        </ul> 
+                    </div>
 
+                    <ul className='sidebar-custom__buttom sidebar-cutom__items-wrapper'>
+                        <a className='big-p' href=""><li>SETTINGS</li></a>
+                        <a className='big-p' href=""><li>LOG OUT</li></a>
+                    </ul> 
+                </div>
         </div>
         </div>
     );
