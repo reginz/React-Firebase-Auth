1c771d9b8d6aff4ff85e77e702c68b70675aebdf
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2022-03-15 11:20:39 +0300

    generate oil

--- src/components/cards/PersonalHealthInfo.js
+++ src/components/cards/PersonalHealthInfo.js
@@ -1,116 +1,85 @@
 import React, { useState } from "react";
 
-function PersonalHealthInfo(props) {
-  const [search, setSearch] = useState("");
+export default function PersonalHealthInfo() {
+  const [state, setState] = useState();
+  const [showResults, setShowResults] = React.useState(false);
 
-  return (
-    <div>
-      <div className=" w-100">
-        <form onSubmit={props.onClick}>
-          <div className="d-flex w-100 align-items-between">
-            <div className="w-50">
-              <div>
-                <small className="small-text">NAME</small>
-                <div>
-                  <hr
-                    style={{ margin: 0, width: "10%" }}
-                    className="orange-hr"
-                  ></hr>
-                </div>
-                <input
-                  ref={props.nameRef}
-                  id="name"
-                  className="mt-3 input-field"
-                  placeholder="eg. User Name"
-                  type="text"
-                ></input>
-              </div>
-
-              <div className="mt-5">
-                <small className="small-text">Age</small>
-                <div>
-                  <hr
-                    style={{ margin: 0, width: "10%" }}
-                    className="orange-hr"
-                  ></hr>
-                </div>
-                <input
-                  ref={props.ageRef}
-                  id="age"
-                  className="mt-3 input-field"
-                  placeholder="eg. 25"
-                  type="text"
-                ></input>
-              </div>
-            </div>
-            {/* <div className="w-50">
-              <small className="small-text">Allergies</small>
-              <div>
-                <hr
-                  style={{ margin: 0, width: "10%" }}
-                  className="orange-hr"
-                ></hr>
-              </div>
-              <input
-                className="input-field w-75 mt-3"
-                type="text"
-                placeholder=" Search Allergies"
-                onChange={(event) => {
-                  setSearch(event.target.value);
-                }}
-              />
-            </div> */}
-          </div>
-
-          {/* <div className=" mt-5">
-            <small className="small-text">effects</small>
-            <div>
-              <hr
-                style={{ margin: 0, width: "10%" }}
-                className="orange-hr"
-              ></hr>
-            </div>
-            <input
-              className="input-field w-75 mt-3"
-              type="text"
-              placeholder="Search Effects"
-              onChange={(event) => {
-                setSearch(event.target.value);
-              }}
-            />
-          </div> */}
-          {props.treatment ? (
-            <div className=" mt-5">
-              <small className="small-text">treatment</small>
-              <div>
-                <hr
-                  style={{ margin: 0, width: "10%" }}
-                  className="orange-hr"
-                ></hr>
-              </div>
-              <input
-                className="input-field w-75 mt-3"
-                type="text"
-                placeholder="Current Treatment"
-                ref={props.treatmentRef}
-              />
-            </div>
-          ) : (
-            <></>
-          )}
-          {props.treatment === false ? (
-            <div className="d-flex justify-content-end">
-              <button id="btn-primary-custom" className="btn mt-3">
-                {props.buttonText}
-              </button>
-            </div>
-          ) : (
-            <></>
-          )}
-        </form>
+  const Results = () => (
+    <div id="results" className="search-results">
+      {state} <br></br>
+      <a
+        href="https://www.aromaterapimarket.com/therapium-naturalis"
+        target="_blank"
+        className="btn-link"
+      >
+        Please visit our website to review our products.
+      </a>
+      <br></br>
+      <div className="d-flex justify-content-end">
+        <button
+          id="btn-primary-custom"
+          className="btn"
+          onClick={() => setShowResults(false)}
+        >
+          Try Again
+        </button>
       </div>
     </div>
   );
-}
+  const ShowForm = () => {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <small className="small-text">Your Complaint</small>
+          <div>
+            <hr style={{ margin: 0, width: "10%" }} className="orange-hr"></hr>
+          </div>
+          <br></br>
+          <textarea
+            id="textarea"
+            className="input-field w-100"
+            required
+            placeholder="EX: I have a headache"
+          ></textarea>
 
-export default PersonalHealthInfo;
+          <br></br>
+
+          <div className="d-flex justify-content-end">
+            <button id="btn-primary-custom" className="btn" type="submit">
+              Generate Blend
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const MonkeyLearn = require("monkeylearn");
+
+    const ml = new MonkeyLearn("7775ad6a7351c42aabe5bfd4bb0d72a7b274a700");
+    let model_id = "cl_ZHru2yjJ";
+    let data = [document.getElementById("textarea").value];
+    ml.classifiers.classify(model_id, data).then((res) => {
+      if (res.body[0].classifications[0]) {
+        var response = res.body[0].classifications[0].tag_name;
+        setState(
+          "Our special recommendation for you is" +
+            " '" +
+            response +
+            "' " +
+            "oil. "
+        );
+      } else {
+        setState(
+          "We could not find a suitable recommendation for you. Please try to describe your request with different sentences."
+        );
+      }
+
+      setShowResults(true);
+    });
+  }
+
+  return <div>{showResults ? <Results /> : <ShowForm />}</div>;
+}
